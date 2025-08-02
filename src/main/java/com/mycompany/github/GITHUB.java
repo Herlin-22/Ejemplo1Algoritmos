5f367438d5ace2f0cdf960f54f10fc1a6d785a17
Herlin <Herlin@192.168.34.54>
2025-08-02 17:04:07 -0600

    "Cambios 02-08-2025"

---
+++ src/main/java/com/mycompany/github/GITHUB.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.github;
+
+/**
+ *
+ * @author Herlin
+ */
+public class GITHUB {
+
+    public static void main(String[] args) {
+        Strinig nombre,edad;
+        Scanner lector=new Scanner (System.in);
+        System.out.println("Ingrese su nombre");
+        nombre=lector.next ();
+        System.out.println("Ingrese Edad");
+        edad=lector.next();
+    }
+}
